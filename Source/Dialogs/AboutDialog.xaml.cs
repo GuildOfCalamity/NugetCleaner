603fe8b62db7414fdad20815b308fde0708ae6b0
GuildOfCalamity <81915293+GuildOfCalamity@users.noreply.github.com>
2025-01-24 17:26:31 -0500

    added spinner

--- Source/Dialogs/AboutDialog.xaml.cs
+++ Source/Dialogs/AboutDialog.xaml.cs
@@ -6,5 +6,22 @@ namespace NugetCleaner.Dialogs;
 
 public sealed partial class AboutDialog : ContentDialog
 {
-    public AboutDialog() => this.InitializeComponent();
+    public AboutDialog()
+    {
+        this.InitializeComponent();
+        this.Loaded += AboutDialogOnLoaded;
+        this.Unloaded += AboutDialogOnUnloaded;
+    }
+
+    void AboutDialogOnUnloaded(object sender, RoutedEventArgs e)
+    {
+        if (!App.IsClosing)
+            StoryboardSpin?.Stop();
+    }
+
+    void AboutDialogOnLoaded(object sender, RoutedEventArgs e)
+    {
+        if (!App.IsClosing)
+            StoryboardSpin?.Begin();
+    }
 }
